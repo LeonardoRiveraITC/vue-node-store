0e4664c894e7ace0fb0bfb1ff001457aa405e68f
LeonardoRiveraITC <19030342@itcelaya.edu.mx>
2023-05-21 16:01:29 -0600

    Add:Se completa tarjeta preview de producto

--- src/components/ProductCardSm.vue
+++ src/components/ProductCardSm.vue
@@ -1,31 +1,48 @@
 <template>
-<v-card
-    class="mx-auto"
-    max-width="344" 
-  >
-    <v-card-text>
-      <div>Seller</div>
-      <p class="text-h4 text--primary">
-      {{product.name}} 
-      </p>
-      <p>{{product.rev}}</p>
-      <div class="text--primary">
-          {{product.desc}}
-      </div>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn
-        variant="text"
-        color="blue"
-        @click=""
-      >
-       Añadir a la cesta 
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+    <v-container>
+        <v-card
+            class="mx-auto"
+            max-width="350" 
+            max-height="400" 
+            > 
+            <v-row no-gutters>
+                <v-col>
+                    <v-card-text>
+                        <div>Seller</div>
+                        <p class="text-h4 text--primary">
+                        {{product.name}} 
+                        </p>
+                        <p>{{product.rev}}</p>
+                        <div class="text--primary">
+                            {{product.desc}}
+                        </div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-btn
+                            variant="text"
+                            color="blue"
+                            @click=""
+                            >
+                            Añadir a la cesta 
+                        </v-btn>
+                    </v-card-actions>
+                </v-col>
+                <v-col>
+                    <v-avatar
+                        class="ma-3"
+                        size="125"
+                        rounded="0"
+                        >
+                        <v-img src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"></v-img>
+                    </v-avatar>
+                </v-col>
+            </v-row>
+        </v-card>
+    </v-container>
 </template>
 <script setup>
-import {ref,computed,onMounted} from 'vue';
+    import {ref,computed,onMounted} from 'vue';
 const props = defineProps({
 product: {},
 })
